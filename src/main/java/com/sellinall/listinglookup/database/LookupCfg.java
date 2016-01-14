ff2adbf9ce6f92c10c2034a7cf05118caca1b8bf
Senthil <senthil@sellinall.com>
2016-01-14 14:40:06 +0800

    fix deprecated mongodb method

--- src/main/java/com/sellinall/listinglookup/database/LookupCfg.java
+++ src/main/java/com/sellinall/listinglookup/database/LookupCfg.java
@@ -1,24 +1,30 @@
 package com.sellinall.listinglookup.database;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.mongodb.MongoDbFactory;
-import org.springframework.data.mongodb.core.MongoTemplate;
-import org.springframework.data.mongodb.core.SimpleMongoDbFactory;
 
+import com.mongodb.DB;
 import com.mongodb.MongoClient;
+import com.mongodb.MongoCredential;
+import com.mongodb.ServerAddress;
 import com.sellinall.listinglookup.config.Config;
 
 @Configuration
 public class LookupCfg {
-	public @Bean MongoDbFactory dbFactory() throws Exception {
-		return new SimpleMongoDbFactory(new MongoClient(Config.getConfig().getLookupCollectionHostName(),
-				Integer.parseInt(Config.getConfig().getLookupCollectionPort())), Config.getConfig()
-				.getLookupCollectionDBName());
-	}
+	public @Bean DB db() throws Exception {
+		List<ServerAddress> seeds = new ArrayList<ServerAddress>();
+		seeds.add(new ServerAddress(Config.getConfig().getLookupCollectionHostName(), Integer.parseInt(Config
+				.getConfig().getLookupCollectionPort())));
 
-	public @Bean MongoTemplate mongoTemplate() throws Exception {
-		MongoTemplate mongoTemplate = new MongoTemplate(dbFactory());
-		return mongoTemplate;
+		List<MongoCredential> credentials = new ArrayList<MongoCredential>();
+		credentials.add(MongoCredential.createScramSha1Credential(Config.getConfig().getDbUserName(), Config
+				.getConfig().getLookupCollectionDBName(), Config.getConfig().getDbPassword().toCharArray()));
+
+		MongoClient mongoClient = new MongoClient(seeds, credentials);
+		DB db = mongoClient.getDB(Config.getConfig().getLookupCollectionDBName());
+		return db;
 	}
 }
