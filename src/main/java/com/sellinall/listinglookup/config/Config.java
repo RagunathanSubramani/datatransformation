6293ae4121c0b966de9427bfb33e2f15a1d91484
deepak <deepak@sellinall.com>
2016-04-26 10:09:58 +0530

    item specification for shopclues(recloned and commited again)

--- src/main/java/com/sellinall/listinglookup/config/Config.java
+++ src/main/java/com/sellinall/listinglookup/config/Config.java
@@ -17,6 +17,70 @@ public class Config {
 	private String LookupCollectionPort;
 	private String LookupCollectionDBName;
 
+	private String shopcluesItemSpecificationUrl;
+	private String shopcluesAuthUrl;
+	private String username;
+	private String password;
+	private String client_id;
+	private String client_secret;
+	private String grant_type;
+
+	public String getGrant_type() {
+		return grant_type;
+	}
+
+	public void setGrant_type(String grant_type) {
+		this.grant_type = grant_type;
+	}
+
+	public String getClient_secret() {
+		return client_secret;
+	}
+
+	public void setClient_secret(String client_secret) {
+		this.client_secret = client_secret;
+	}
+
+	public String getClient_id() {
+		return client_id;
+	}
+
+	public void setClient_id(String client_id) {
+		this.client_id = client_id;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getShopcluesItemSpecificationUrl() {
+		return shopcluesItemSpecificationUrl;
+	}
+
+	public void setShopcluesItemSpecificationUrl(String shopcluesItemSpecificationUrl) {
+		this.shopcluesItemSpecificationUrl = shopcluesItemSpecificationUrl;
+	}
+
+	public String getShopcluesAuthUrl() {
+		return shopcluesAuthUrl;
+	}
+
+	public void setShopcluesAuthUrl(String shopcluesAuthUrl) {
+		this.shopcluesAuthUrl = shopcluesAuthUrl;
+	}
+
 	public String getEbayPostURL() {
 		return EbayPostURL;
 	}
