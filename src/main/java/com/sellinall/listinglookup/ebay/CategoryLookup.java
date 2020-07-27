01b4f5943de5979d6665af3fa56f92b733113e85
sathiya-sellinall <sathiya@sellinall.com>
2020-07-27 12:16:39 +0530

    error log added for ebay get category faliure case

--- src/main/java/com/sellinall/listinglookup/ebay/CategoryLookup.java
+++ src/main/java/com/sellinall/listinglookup/ebay/CategoryLookup.java
@@ -86,43 +86,48 @@ public class CategoryLookup {
 
 	private static JSONObject getCategorySpecificsFromEbay(String countryCode, String categoryId) {
 		String categorySpecificsXML = EBayUtil.getCategorySpecifics(countryCode, categoryId);
-
 		JSONObject categorySpecificsFromEbay = XML.toJSONObject(categorySpecificsXML);
 		JSONObject GetCategorySpecificsResponse = categorySpecificsFromEbay
 				.getJSONObject("GetCategorySpecificsResponse");
-		JSONObject Recommendations = GetCategorySpecificsResponse.getJSONObject("Recommendations");
 		JSONArray NameRecommendation;
-		if (Recommendations.has("NameRecommendation")) {
-			// NameRecommendation can either be a json object (in case of
-			// single element) or json array. Handle accordingly.
-			if (Recommendations.get("NameRecommendation").getClass() == org.json.JSONArray.class) {
-				NameRecommendation = new JSONArray(Recommendations.getJSONArray("NameRecommendation").toString());
-			} else {
-				JSONObject NameRecommendationElement = new JSONObject(Recommendations.getJSONObject(
-						"NameRecommendation").toString());
-				NameRecommendation = new JSONArray();
-				NameRecommendation.put(NameRecommendationElement);
-			}
-			for (int i = 0; i < NameRecommendation.length(); i++) {
-				JSONObject NameRecommendataionObj = NameRecommendation.getJSONObject(i);
-				if (NameRecommendataionObj.has("ValueRecommendation")) {
-					JSONArray ValueRecommendation = new JSONArray();
-					if (NameRecommendataionObj.get("ValueRecommendation").getClass() == org.json.JSONArray.class) {
-						ValueRecommendation = NameRecommendataionObj.getJSONArray("ValueRecommendation");
-					} else {
-						ValueRecommendation.put(NameRecommendataionObj.getJSONObject("ValueRecommendation"));
-					}
-					JSONArray valuesOfRecommendation = new JSONArray();
-					for (int j = 0; j < ValueRecommendation.length(); j++) {
-						valuesOfRecommendation.put(ValueRecommendation.getJSONObject(j).get("Value").toString());
-					}
-					NameRecommendataionObj.put("valuesOfRecommendation", valuesOfRecommendation);
-					NameRecommendation.put(i, NameRecommendataionObj);
+		if (GetCategorySpecificsResponse.has("Recommendations")) {
+			JSONObject Recommendations = GetCategorySpecificsResponse.getJSONObject("Recommendations");
+			if (Recommendations.has("NameRecommendation")) {
+				// NameRecommendation can either be a json object (in case of
+				// single element) or json array. Handle accordingly.
+				if (Recommendations.get("NameRecommendation").getClass() == org.json.JSONArray.class) {
+					NameRecommendation = new JSONArray(Recommendations.getJSONArray("NameRecommendation").toString());
+				} else {
+					JSONObject NameRecommendationElement = new JSONObject(
+							Recommendations.getJSONObject("NameRecommendation").toString());
+					NameRecommendation = new JSONArray();
+					NameRecommendation.put(NameRecommendationElement);
 				}
+				for (int i = 0; i < NameRecommendation.length(); i++) {
+					JSONObject NameRecommendataionObj = NameRecommendation.getJSONObject(i);
+					if (NameRecommendataionObj.has("ValueRecommendation")) {
+						JSONArray ValueRecommendation = new JSONArray();
+						if (NameRecommendataionObj.get("ValueRecommendation").getClass() == org.json.JSONArray.class) {
+							ValueRecommendation = NameRecommendataionObj.getJSONArray("ValueRecommendation");
+						} else {
+							ValueRecommendation.put(NameRecommendataionObj.getJSONObject("ValueRecommendation"));
+						}
+						JSONArray valuesOfRecommendation = new JSONArray();
+						for (int j = 0; j < ValueRecommendation.length(); j++) {
+							valuesOfRecommendation.put(ValueRecommendation.getJSONObject(j).get("Value").toString());
+						}
+						NameRecommendataionObj.put("valuesOfRecommendation", valuesOfRecommendation);
+						NameRecommendation.put(i, NameRecommendataionObj);
+					}
+				}
+			} else {
+				// create empty array when NameRecommendation is not present in
+				// the response from eBay.
+				NameRecommendation = new JSONArray();
 			}
 		} else {
-			// create empty array when NameRecommendation is not present in
-			// the response from eBay.
+			log.error("Recommendations key not available on get category details from ebay for categoryID" + categoryId
+					+ " countryCode:" + countryCode + " and reponse: " + GetCategorySpecificsResponse.toString());
 			NameRecommendation = new JSONArray();
 		}
 		JSONObject categorySpecifics = new JSONObject();
