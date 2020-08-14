a80e911bc6eede938a0671b48ce32c6a5a955b11
sathiya-sellinall <sathiya@sellinall.com>
2020-08-14 15:39:10 +0530

    lazada refresh brand pagination based

--- src/main/java/com/sellinall/listinglookup/lazada/BuildBrand.java
+++ src/main/java/com/sellinall/listinglookup/lazada/BuildBrand.java
@@ -9,6 +9,9 @@ import org.codehaus.jettison.json.JSONException;
 import org.codehaus.jettison.json.JSONObject;
 import org.eclipse.jetty.http.HttpStatus;
 
+import com.mongodb.BasicDBList;
+import com.mongodb.BasicDBObject;
+import com.mongodb.util.JSON;
 import com.sellinall.listinglookup.config.Config;
 import com.sellinall.util.AuthConstant;
 import com.sellinall.util.HttpsURLConnectionUtil;
@@ -20,27 +23,44 @@ public class BuildBrand {
 		header.put(AuthConstant.RAGASIYAM_KEY, Config.getConfig().getRagasiyam());
 		header.put("accountNumber", accountNumber);
 		header.put("Content-Type", "application/json");
-		JSONObject serviceResponse = HttpsURLConnectionUtil
-				.doGet(Config.getConfig().getLazadaURL() + "/brand?nickNameID=" + nickNameID, header);
+		boolean nextPageAvailable = true;
+		int loopCount = 0;
+		int pageLimit = 1000;
+		long offset = 0;
 		String brandList = "";
-		if (serviceResponse.getInt("httpCode") == HttpStatus.OK_200) {
-			JSONObject response = new JSONObject(serviceResponse.getString("payload"));
-			response = new JSONObject(response.getString("data"));
-			if (response.has("code") && response.getString("code").equals("0")) {
-				brandList = parseAndPrintFile((JSONArray) response.get("data"));
+		BasicDBList overAllResponse = new BasicDBList();
+		do {
+			if (loopCount != 0) {
+				offset = offset + pageLimit;
 			}
-		}
+			nextPageAvailable = false;
+			JSONObject serviceResponse = HttpsURLConnectionUtil.doGet(Config.getConfig().getLazadaURL()
+					+ "/brand?nickNameID=" + nickNameID + "&offset=" + offset + "&pageLimit=" + pageLimit, header);
+			if (serviceResponse.getInt("httpCode") == HttpStatus.OK_200) {
+				JSONObject response = new JSONObject(serviceResponse.getString("payload"));
+				response = new JSONObject(response.getString("data"));
+				if (response.has("code") && response.getString("code").equals("0")) {
+					JSONArray data = (JSONArray) response.get("data");
+					overAllResponse.addAll((BasicDBList) JSON.parse(data.toString()));
+					if (data.length() == pageLimit) {
+						nextPageAvailable = true;
+					}
+				}
+			}
+			loopCount++;
+		} while (nextPageAvailable);
+		brandList = parseAndPrintFile(overAllResponse);
 		return brandList;
 	}
 
-	private static String parseAndPrintFile(JSONArray jsonArray) throws JSONException {
+	private static String parseAndPrintFile(BasicDBList overAllResponse) {
 		JSONArray brandString = new JSONArray();
-		for (int i = 0; i < jsonArray.length(); i++) {
-			JSONObject brand = jsonArray.getJSONObject(i);
-			if (brand.has("brand_id")) {
-				if (brand.has("name_en")) {
+		for (int i = 0; i < overAllResponse.size(); i++) {
+			BasicDBObject brand = (BasicDBObject) overAllResponse.get(i);
+			if (brand.containsField("brand_id")) {
+				if (brand.containsField("name_en")) {
 					brandString.put(brand.getString("name_en") + " ## " + brand.getString("brand_id"));
-				} else if (brand.has("name")) {
+				} else if (brand.containsField("name")) {
 					brandString.put(brand.getString("name") + " ## " + brand.getString("brand_id"));
 				}
 			}
